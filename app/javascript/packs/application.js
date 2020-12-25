1db97943748e984388980da1289e4832a3666124
Martyna Markiel <martyna.markiel@gmail.com>
2020-12-25 15:56:33 +0100

    jquery update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,38 +5,17 @@ require("channels")
 
 
 // External imports
+import "bootstrap";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-AOS.init();
-
-import "bootstrap";
 
 import { loadDynamicBannerText } from '../components/banner';
-
-document.addEventListener('turbolinks:load', () => {
-
-  loadDynamicBannerText();
-
-});
-
-
 import { initRellax } from '../components/rellax.js';
 
-document.addEventListener('turbolinks:load', () => {
-  // Call your JS functions here
-  // ANIMATE ON SCROLL INIT
-  AOS.init();
-  // [...]
-if (document.querySelector('.rellax')) {
-    initRellax();
-  };
-
-});
-
-// Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
+  // JQUERY GLOBAL $ FOR AJAX
+  global.$ = jQuery;
 
   // WELCOME MESSAGE
   console.log('%c%s',
@@ -46,12 +25,15 @@ document.addEventListener('turbolinks:load', () => {
   console.log('%c%s',
               "color: #FFF; background: #6086A8; font-family: 'courier'; padding: .25rem;",
               "I will be happy to connect https://github.com/mrtn-a");
-  // Call your functions here, e.g:
-  // initSelect2();
+
+  loadDynamicBannerText();
+
+  // ANIMATE ON SCROLL INIT
+  AOS.init();
 
   //RELLAX INIT
-  // if (document.querySelector('.rellax')) {
-  //   initRellax();
-  // };
+  if (document.querySelector('.rellax')) {
+    initRellax();
+  };
 
 });
